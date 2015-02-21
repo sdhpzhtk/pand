cd181020c02fb53c821e32071eb0a27005c8bb5b
Xander Zheng <tzheng@caltech.edu>
2015-02-20 19:42:30 -0800

    Implemented basic IO and degree selection.

---
+++ select.py
@@ -0,0 +1,58 @@
+import json
+from sys import *
+
+def read_graph(fname):
+    ''' Reads a graph json file. Note the file should be under graph/
+    
+    Args:
+        fname: string. The file name of the graph under the folder graph/.
+    
+    Returns
+        A dict of adjacency lists.
+    '''
+    with open('graph/' + fname, 'r') as fin:
+        data = json.load(fin)
+
+    return data
+
+def write_seed(alg_name, fname, seeds):
+    ''' Writes the selected seeds to file.
+    
+    Args:
+        alg_name: the algorithm name, the file will be saved under alg_name 
+                  folder
+        fname: the file name
+        seeds: [50 * num_seeds of seed id's]. Note seed id should be string.
+    '''
+    with open(alg_name + '/' + fname, 'w') as fout:
+        for seed in seeds:
+            fout.write(seed + '\n')
+
+def deg(adj_dict, n):
+    ''' Chooses n nodes based on the most degrees.
+    
+    Args:
+        adj_dict: a dict of {seed_id: a list of adjacent seed_id's}
+        
+    Returns:
+        seeds: 50 * [list of top n max-degree seed_id's]
+    '''
+
+    degs = sorted([(id, len(adj_dict[id])) for id in adj_dict],
+                  key=lambda x: x[1])
+    seeds = [degs[i][0] for i in range(n)]
+    return seeds * 50
+
+# A dict of all algorithms for selection. {flag: (folder name, function name)}
+algs = {'d': ('deg', deg)}
+
+if __name__ == '__main__':
+    g_name = argv[1]
+    n = int(g_name.split('.')[1])
+    graph = read_graph(g_name)
+    
+    for flag in argv[2:]:
+        if flag in algs:
+            folder, f = algs[flag]
+            seeds = f(graph, n)
+            write_seed(folder, g_name, seeds)
